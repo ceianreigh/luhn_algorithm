eacf3b7e28649d4766535859d4b83e433e3572cd
ceianreigh <ceianreighkeannmcepillo@iskolarngbayan.pup.edu.ph>
2024-10-10 09:15:15 +0800

    define the main function

--- luhn_algorithm.py
+++ luhn_algorithm.py
@@ -1,1 +1,9 @@
 # validating atm card number with luhn algorithm
+
+
+def main():
+    card_number = input("Enter card number: ")
+    card_translation = str.maketrans({'-': '', ' ': ''})
+    translated_card_number = card_number.translate(card_translation)
+
+main()
