7a2637a0ee073fca3239c50244f9c98c6e4573f9
ceianreigh <ceianreighkeannmcepillo@iskolarngbayan.pup.edu.ph>
2024-10-10 09:29:35 +0800

    add comment

--- luhn_algorithm.py
+++ luhn_algorithm.py
@@ -1,9 +1,12 @@
 # validating atm card number with luhn algorithm
 def verify_card_number(card_number):
-    pass
+    reversed_card_number = card_number[::-1]
+
 
 def main():
     card_number = input("Enter card number: ")
+
+    # remove spaces and hyphens from card number
     card_translation = str.maketrans({'-': '', ' ': ''})
     translated_card_number = card_number.translate(card_translation)
 
